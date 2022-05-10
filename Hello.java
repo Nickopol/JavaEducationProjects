8bf4b0d4d0253748fa5de12a6eb36b823d9db5e1
Nickopol <35582979+Nickopol@users.noreply.github.com>
2022-05-10 12:56:30 +0300

    Hello.java
    Add new file

---
+++ Hello.java
@@ -0,0 +1,9 @@
+package JavaEducationProjects;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hellow you!");
+        System.out.println("Hello.main");
+    }
+}
