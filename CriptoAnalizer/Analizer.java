74bfe93a4305dc3b0b50cab46d38c8921b2e3f51
Andry <reddemonster@gmail.com>
2022-07-21 18:08:26 +0300

    Task CrAn. Add coments

---
+++ CriptoAnalizer/Analizer.java
@@ -0,0 +1,18 @@
+package CriptoAnalizer;
+
+public class Analizer {
+    /*
+    In this task we need to analize text on subject of encryption of text.
+    Encriptor is a number, on which Char number of symbol will change in encripted text.
+
+    Class Opening  -  for opening new file.
+    Class encryption - for analazing text and coded it to char Array.
+    Class PushSaving - for creating new file of encriped text.
+    Class Dencrypted - for writing in new file decripted text.
+
+     */
+
+
+
+
+}
