ab505d1c1b5ca643cf6741aca49cbcc7217f5996
Andry <reddemonster@gmail.com>
2022-07-21 19:14:12 +0300

    Task CrAn. Add main logic. And add file for encripting.

--- CriptoAnalizer/Analizer.java
+++ CriptoAnalizer/Analizer.java
@@ -5,14 +5,45 @@ public class Analizer {
     In this task we need to analize text on subject of encryption of text.
     Encriptor is a number, on which Char number of symbol will change in encripted text.
 
-    Class Opening  -  for opening new file.
-    Class encryption - for analazing text and coded it to char Array.
-    Class PushSaving - for creating new file of encriped text.
-    Class Dencrypted - for writing in new file decripted text.
-
+    Char Array openingFileArray  -  for opening new file.
+    Char Array encryption - for analazing text and coded it to char Array.
+    Void pushSaving - for creating new file of encriped text.
+    Void dencrypting - for writing in new file decripted text.
+                When program end`s in cosole must be messege All done.
      */
+    private  static  final int displacementNumber = 3;
+    private  static  String incomeTextFileName = "To Encription.txt";
+    private  static  String outcomeTextFileName = "Encripted text.txt";
+    private  static  String decriptedTextFileName = "Dencripted text.txt";
+
+    public static void main(String[] args) {
+        //pushSaving(encryption(openingFileArray()));
+        //dencrypting();
+    }
+
+    private static char[] openingFileArray () {
+         //char[] outcome = null;
+        return null;
+        // return outcome;
+    }
+
+    private static char[] encryption (char[] incomeTextArray) {
+       // char[] outcome = null;
+        return null;
+        // return outcome;
+    }
+
+    private static void pushSaving (char[] textForSaving) {
 
 
+        System.out.println("All Done.");
+    }
+
+    private static void dencrypting () {
+
+
+        System.out.println("All Done.");
+    }
 
 
 }
