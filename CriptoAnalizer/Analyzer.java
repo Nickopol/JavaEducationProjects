6d8616edea0c57b3a48a51dd431a0b88cb57e6a3
reddemonster <reddemonster@gmail.com>
2022-08-07 14:14:20 +0000

    Final refactor

---
+++ CriptoAnalizer/Analyzer.java
@@ -0,0 +1,17 @@
+package CriptoAnalizer;
+
+import CriptoAnalizer.controller.ConsoleController;
+
+/*
+    In this task we need to analyze text on subject of encryption of text.
+    Encryptor is a number, on which Char number of symbol will change in encrypted text.
+
+    */
+public class Analyzer {
+
+    public static void main(String[] args) {
+
+        ConsoleController cc = new ConsoleController();
+        cc.callMainManu();
+    }
+}
